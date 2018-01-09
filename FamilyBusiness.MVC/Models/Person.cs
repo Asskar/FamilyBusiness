f6a99c8369a102df1b40e3fa769176b9e71d193d
Michał Kiliński <michal.kilinski@onexgroup.pl>
2018-01-09 22:42:01 +0100

    Expand person model

--- FamilyBusiness.MVC/Models/Person.cs
+++ FamilyBusiness.MVC/Models/Person.cs
@@ -2,6 +2,19 @@
 {
     public class Person
     {
-        
+        public string Id { get; set; }
+        public string Pesel { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthCountry { get; set; }
+        public string BirthCity { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+        public string Company { get; set; }//todo: more data for company
+        public string ExaminatedPosition { get; set; }
     }
 }
